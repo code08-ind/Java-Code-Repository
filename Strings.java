99a5a4671a6a3ee48b66f28e20f60d4505dc3394
Aryan Garg <69002338+code08-ind@users.noreply.github.com>
2025-08-15 17:18:24 +0530

    Create Strings.java

---
+++ Strings.java
@@ -0,0 +1,9 @@
+public class Strings {
+    public static void main (String[] args){
+// Strings are reference types in Java but in the shorter mode, we can assign it just like the primitive type
+        String str = new String("Hello");
+//        There are various methods present with the Strings. Strings are immutable in Java.
+
+//        There are some escape chars we use: \", \\, \n, \t
+    }
+}
