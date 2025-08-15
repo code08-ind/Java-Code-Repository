a3d91e52d85855723d0b677fda16c117a5013748
Aryan Garg <69002338+code08-ind@users.noreply.github.com>
2025-08-15 16:33:55 +0530

    Create Intro.java

---
+++ Intro.java
@@ -0,0 +1,13 @@
+public class Intro {
+//    the main method should always be static here
+    public static void main(String[] args) {
+//        System is built in class where out is a member of it
+        System.out.println("Hello world!");
+    }
+}
+
+/*
+- Java Code is converted to the Byte Code by the compiler and compiler comes up with the jdk kit. (.class)
+- This Bytecode is Platform Independent.
+- We have jre and it jvm converts .class to native code, and we can execute the code and can get the output.
+ */
