7d687022f71d7f0e404edf011cd0b1df4f4b9366
Aryan Garg <69002338+code08-ind@users.noreply.github.com>
2025-08-17 15:14:34 +0530

    Update MortgageCalculator.java by adding validations

--- MortgageCalculator.java
+++ MortgageCalculator.java
@@ -4,20 +4,43 @@ import java.util.Scanner;
 public class MortgageCalculator {
     public static void main(String[] args) {
         double mortgage;
-        double annualRateOfInterest;
+        double annualRateOfInterest = 0;
         double monthlyRateOfInterest;
-        double principal;
-        double years;
+        double principal = 0;
+        int years = 0;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Principal: ");
-        principal = sc.nextDouble();
-        System.out.print("Annual Rate of Interest: ");
-        annualRateOfInterest = sc.nextDouble();
-        monthlyRateOfInterest = annualRateOfInterest/1200;
-        System.out.print("Period (Years): ");
-        years = sc.nextDouble();
-        double monthlyPayments = 12 * years;
-        mortgage = principal * (monthlyRateOfInterest * Math.pow(1+monthlyRateOfInterest, monthlyPayments))/(Math.pow(1+monthlyRateOfInterest, monthlyPayments)-1);
+
+        while (!(principal >= 1000 && principal <= 1_000_000)) {
+            System.out.print("Principal ($1K - $1M): ");
+            principal = sc.nextDouble();
+            if (!(principal >= 1000 && principal <= 1_000_000)) {
+                System.out.println("Enter a number between 1,000 and 1,000,000");
+            }
+        }
+
+        while (!(annualRateOfInterest > 0 && annualRateOfInterest <= 30)) {
+            System.out.print("Annual Rate of Interest (%): ");
+            annualRateOfInterest = sc.nextDouble();
+            if (!(annualRateOfInterest > 0 && annualRateOfInterest <= 30)) {
+                System.out.println("Enter a value greater than 0 and less than or equal to 30");
+            }
+        }
+
+        while (!(years >= 1 && years <= 30)) {
+            System.out.print("Period (Years): ");
+            years = sc.nextInt();
+            if (!(years >= 1 && years <= 30)) {
+                System.out.println("Enter a value between 1 and 30");
+            }
+        }
+
+        monthlyRateOfInterest = annualRateOfInterest / 1200; // annual % → monthly decimal
+        int numberOfPayments = years * 12;
+
+        mortgage = principal *
+                (monthlyRateOfInterest * Math.pow(1 + monthlyRateOfInterest, numberOfPayments)) /
+                (Math.pow(1 + monthlyRateOfInterest, numberOfPayments) - 1);
+
         System.out.println("Mortgage: " + NumberFormat.getCurrencyInstance().format(mortgage));
     }
 }
