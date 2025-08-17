35af46f4f2422f9457366875de8a2afe68f1e85d
Aryan Garg <69002338+code08-ind@users.noreply.github.com>
2025-08-17 16:38:45 +0530

    Create ErrorTypes.java

---
+++ ErrorTypes.java
@@ -0,0 +1,17 @@
+public class ErrorTypes {
+    public static void main(String[] args) {
+//        We have two types of errors:
+        /*
+        - Compile Time Errors/Syntax Errors
+        - Runtime Errors: These can be found using the Debuggers.
+
+        - Common Compile Time Errors
+            - Not defining a variable
+            - semicolon
+            - forgot to add parenthesis in the method calls
+            - forgot to add the quotes inside the string declaration
+            - Always make sure we consider case-sensitive behavior of variables
+            - Using reserved keywords in Java
+        */
+    }
+}
